244b3efd5699a872b4182c62b894d49f1f5045e6
Tuhin Samanta <tuhinsamantastudent@gmail.com>
2023-03-21 18:24:15 +0530

    Modifications

--- src/controllers/jokes.controller.ts
+++ src/controllers/jokes.controller.ts
@@ -1,8 +1,17 @@
 // Uncomment these imports to begin using these cool features!
 
-// import {inject} from '@loopback/core';
+import {get, post} from '@loopback/rest';
 
+// import {inject} from '@loopback/core';
 
 export class JokesController {
   constructor() {}
+
+  @get('/test')
+  async test(): Promise<string> {
+    return 'Ok';
+  }
+
+  @post('/jokes')
+  getJokes() {}
 }
